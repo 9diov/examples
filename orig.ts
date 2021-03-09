456ebb57966c754a4760f65fca8427152b0f595d
Thanh Dinh Khac <thanhdk@gmail.com>
2021-03-09 10:10:11 +0700

    Introducing some changes

--- orig.ts
+++ orig.ts
@@ -3,25 +3,47 @@ class OriginalClassA {
   data2: number;
   option1: boolean;
   option2: number;
+  option3: boolean;
 
-  constructor(data1: string, data2: number, option1: boolean, option2: number) {
+  constructor(data1: string, data2: number, option1: boolean, option2: number, option3: boolean) {
     this.data1 = data1;
     this.data2 = data2;
     this.option1 = option1;
     this.option2 = option2;
+    this.option3 = option3;
   }
 
-  doX(optionX: boolean): void {
+  doX(optionX: boolean, option4: number): void {
+    // old code
+    // ...
+
+    // new code
+    if (this.option3) {
+      if (option4 > 0) {
+        console.log("Do something new");
+      } else {
+        console.log("Do something crazily new");
+      }
+    }
   }
 
-  doY(optionY: number): void {
+  doY(optionY: number, option5: string): void {
     const b = new OriginalClassB();
-    b.doZ();
+    b.doZ(option5);
   }
 }
 
 class OriginalClassB {
-  doZ(): void {
+  doZ(option5: string): void {
+    // old code
+    let x = "hahaha";
+
+    // new code
+    if (option5 == 'blah') {
+      // mutate old code
+      x = x + option5;
+      console.log("Mutating is fun!?!?")
+    }
   }
 
   doT(): void {
